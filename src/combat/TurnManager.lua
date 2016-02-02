fb085f3171dd80db49653f7441fd439869e6553a
Robert Machmer <robert.machmer@gmail.com>
2016-02-02 03:14:26 +0100

    Remove key controls

--- src/combat/TurnManager.lua
+++ src/combat/TurnManager.lua
@@ -61,24 +61,6 @@ function TurnManager.new( map, characters )
     end
 
     function self:keypressed( key )
-        if key == 'w' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.NORTH ));
-        elseif key == 'x' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.SOUTH ));
-        elseif key == 'a' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.WEST ));
-        elseif key == 'd' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.EAST ));
-        elseif key == 'q' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.NORTH_WEST ));
-        elseif key == 'e' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.NORTH_EAST ));
-        elseif key == 'y' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.SOUTH_WEST ));
-        elseif key == 'c' then
-            actionQueue:enqueue( Walk.new( characters[characterIndex], DIRECTION.SOUTH_EAST ));
-        end
-
         if key == 'space' then
             endTurn();
         end
