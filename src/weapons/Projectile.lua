7739091538a3d1883c21c0e4f09fee9f74a34260
Robert Machmer <robert.machmer@gmail.com>
2016-02-28 15:50:32 +0100

    Use a normalized vector when moving projectiles
    This way projectiles have the same speed independent from the target
    distance.

--- src/weapons/Projectile.lua
+++ src/weapons/Projectile.lua
@@ -1,14 +1,18 @@
 local Object = require( 'src.Object' );
 
+local SPEED = 10;
+
 local Projectile = {};
 
 function Projectile.new( character, px, py, tx, ty )
     local self = Object.new():addInstance( 'Projectile' );
 
     local dx, dy = tx - px, ty - py;
+    local magnitude = math.sqrt( dx * dx + dy * dy );
+    local normalisedX, normalisedY = dx / magnitude, dy / magnitude;
 
     function self:update( dt )
-        px, py = px + dx * dt * 2, py + dy * dt * 2;
+        px, py = px + normalisedX * dt * SPEED, py + normalisedY * dt * SPEED;
     end
 
     function self:getCharacter()
