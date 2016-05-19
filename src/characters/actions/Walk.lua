1d1afa302574ee7c6e0ea3924d3671755fbb9eb7
Robert Machmer <robert.machmer@gmail.com>
2016-05-19 16:42:20 +0200

    Use tile's movement cost for walk action
    The cost of a walking action is now determined by the tile it is
    performed on.

--- src/characters/actions/Walk.lua
+++ src/characters/actions/Walk.lua
@@ -3,7 +3,7 @@ local Action = require('src.characters.actions.Action');
 local Walk = {};
 
 function Walk.new( character, target )
-    local self = Action.new( 1 ):addInstance( 'Walk' );
+    local self = Action.new( target:getMovementCost() ):addInstance( 'Walk' );
 
     function self:perform()
         local current = character:getTile();
