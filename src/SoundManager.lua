a79ce760d6cf2795d615c14a6bf051317222feb2
Robert Machmer <robert.machmer@gmail.com>
2016-05-26 11:56:43 +0200

    Stop the Source for firing a shot before playing
    This makes sure the sound is played for every shot the character fired.
    Without this the game would wait for the Source to finish, before
    playing it again.

--- src/SoundManager.lua
+++ src/SoundManager.lua
@@ -8,12 +8,24 @@ local SOUNDS = {
     SHOOT  = love.audio.newSource( 'res/sounds/shoot.wav' );
 }
 
+---
+-- Stops a source if it is currently playing.
+-- @param source (Source) The source to stop.
+-- @return       (Source) The stopped source.
+--
+local function stopBeforePlaying( source )
+    if source:isPlaying() then
+        source:stop();
+    end
+    return source;
+end
+
 Messenger.observe( 'ACTION_DOOR', function()
     love.audio.play( SOUNDS.DOOR );
 end)
 
 Messenger.observe( 'ACTION_SHOOT', function()
-    love.audio.play( SOUNDS.SHOOT );
+    love.audio.play( stopBeforePlaying( SOUNDS.SHOOT ));
 end)
 
 Messenger.observe( 'SWITCH_CHARACTERS', function()
