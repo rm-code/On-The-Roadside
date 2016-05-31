83573ec2ac368a817da5a8c20b3ed74681b90878
Robert Machmer <robert.machmer@gmail.com>
2016-05-31 19:46:25 +0200

    Make ItemFactory more flexible
    This will help us to use it for different item types later on.

--- src/items/ItemFactory.lua
+++ src/items/ItemFactory.lua
@@ -1,24 +1,36 @@
 local Weapon = require( 'src.items.weapons.Weapon' );
 
+local TEMPLATES_DIRECTORY_WEAPONS  = 'res/data/items/weapons/';
+
 local ItemFactory = {};
 
-local weapons = {};
+local items = {};
 
-function ItemFactory.loadTemplates()
-    local dir = 'res/data/items/weapons/';
-
+local function load( dir )
     local files = love.filesystem.getDirectoryItems( dir );
-    print("Load Weapon Templates: ");
     for i, file in ipairs( files ) do
         if love.filesystem.isFile( dir .. file ) then
-            weapons[#weapons + 1] = love.filesystem.load( dir .. file )();
-            print( "  " .. i .. ". " .. file .. " - " .. weapons[#weapons].name );
+            local template = love.filesystem.load( dir .. file )();
+            local itemType = template.itemType;
+
+            items[itemType] = items[itemType] or {};
+
+            table.insert( items[itemType], template );
+
+            print( string.format( '  %d. %s', i, template.name ));
         end
     end
 end
 
+function ItemFactory.loadTemplates()
+    print( "Load Weapon Templates:" )
+    load( TEMPLATES_DIRECTORY_WEAPONS );
+end
+
 function ItemFactory.createWeapon()
-    return Weapon.new( weapons[love.math.random( 1, #weapons )] );
+    local rnd = love.math.random( 1, #items.Weapon );
+    local template = items.Weapon[rnd];
+    return Weapon.new( template );
 end
 
 return ItemFactory;
