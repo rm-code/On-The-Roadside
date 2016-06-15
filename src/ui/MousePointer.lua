3c9becd92f3ad4c9e81bb0ca4577aff86b567f8c
Robert Machmer <robert.machmer@gmail.com>
2016-06-15 11:40:51 +0200

    Center mouse cursor when game starts

--- src/ui/MousePointer.lua
+++ src/ui/MousePointer.lua
@@ -4,6 +4,8 @@ local TILE_SIZE = require( 'src.constants.TileSize' );
 
 local camera;
 
+love.mouse.setPosition( love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5 );
+
 function MousePointer.init( ncamera )
     camera = ncamera;
 end
