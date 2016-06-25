881be7f6b270ee5d8771ad34f47538f6c244ebb5
Robert Machmer <robert.machmer@gmail.com>
2016-06-25 11:28:10 +0200

    Refactor Map.lua

--- src/map/Map.lua
+++ src/map/Map.lua
@@ -1,7 +1,12 @@
 local Object = require( 'src.Object' );
-
 local TileFactory = require( 'src.map.tiles.TileFactory' );
 local WorldObjectFactory = require( 'src.map.worldobjects.WorldObjectFactory' );
+
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
+
+local Map = {};
 
 -- ------------------------------------------------
 -- Constants
@@ -13,7 +18,9 @@ local INFO_FILE = love.filesystem.load( 'res/data/maps/info.lua' )();
 local GROUND_LAYER = love.image.newImageData( 'res/data/maps/Map_Ground.png' );
 local OBJECT_LAYER = love.image.newImageData( 'res/data/maps/Map_Objects.png' );
 
-local Map = {};
+-- ------------------------------------------------
+-- Constructor
+-- ------------------------------------------------
 
 function Map.new()
     local self = Object.new():addInstance( 'Map' );
@@ -21,9 +28,19 @@ function Map.new()
     local tiles;
 
     -- ------------------------------------------------
-    -- Public Functions
+    -- Private Methods
     -- ------------------------------------------------
 
+    ---
+    -- Creates a Tile at the given coordinates based on the pixel color read
+    -- from the map's ground layer.
+    -- @param x (number) The tile's coordinate along the x-axis.
+    -- @param y (number) The tile's coordinate along the y-axis.
+    -- @param r (number) The red-component read from the ground layer.
+    -- @param g (number) The green-component read from the ground layer.
+    -- @param b (number) The blue-component read from the ground layer.
+    -- @return  (Tile)   The new Tile object.
+    --
     local function createTile( x, y, r, g, b, _ )
         local tile;
         for _, info in ipairs( INFO_FILE.ground ) do
@@ -35,7 +52,17 @@ function Map.new()
         return TileFactory.create( x, y, tile );
     end
 
+    ---
+    -- Creates a WorldObject on the given Tile based on the pixel color read
+    -- from the map's object layer.
+    -- @param tile (Tile)        The Tile on which to place the WorldObject.
+    -- @param r    (number)      The red-component read from the object layer.
+    -- @param g    (number)      The green-component read from the object layer.
+    -- @param b    (number)      The blue-component read from the object layer.
+    -- @param a    (number)      The alpha value read from the object layer.
+    --
     local function createWorldObject( tile, r, g, b, a )
+        -- Ignore transparent pixels.
         if a == 0 then
             return;
         end
@@ -50,19 +77,26 @@ function Map.new()
         tile:addWorldObject( WorldObjectFactory.create( object ));
     end
 
+    ---
+    -- Iterates over the ground layer's RGBA values pixel by pixel and creates
+    -- tiles based on the loaded colors.
+    -- @return (table) A 2d array containing the map's tiles.
+    --
     local function createTiles()
         local newTiles = {};
-
         for x = 1, GROUND_LAYER:getWidth() do
             for y = 1, GROUND_LAYER:getHeight() do
                 newTiles[x] = newTiles[x] or {};
                 newTiles[x][y] = createTile( x, y, GROUND_LAYER:getPixel( x - 1, y - 1 ));
             end
         end
-
         return newTiles;
     end
 
+    ---
+    -- Iterates over the object layer's RGBA values pixel by pixel and creates
+    -- WorldObjects based on the loaded colors.
+    --
     local function createWorldObjects()
         for x = 1, OBJECT_LAYER:getWidth() do
             for y = 1, OBJECT_LAYER:getHeight() do
@@ -94,15 +128,23 @@ function Map.new()
     end
 
     -- ------------------------------------------------
-    -- Public Functions
+    -- Public Methods
     -- ------------------------------------------------
 
+    ---
+    -- Initialises the map by creating the Tiles, creating references to the
+    -- neighbouring tiles and adding WorldObjects.
+    --
     function self:init()
         tiles = createTiles();
         createWorldObjects();
         addNeighbours();
     end
 
+    ---
+    -- Iterates over all tiles and performs the callback function on them.
+    -- @param callback (function) The operation to perform on each tile.
+    --
     function self:iterate( callback )
         for x = 1, #tiles do
             for y = 1, #tiles[x] do
@@ -112,7 +154,8 @@ function Map.new()
     end
 
     ---
-    -- Resets the visibility flags for all visible tiles in the map.
+    -- Resets the visibility flags for all visible tiles and marks them for
+    -- a drawing update.
     --
     function self:resetVisibility()
         self:iterate( function( tile )
@@ -124,8 +167,10 @@ function Map.new()
     end
 
     ---
-    -- Cast rays in a 360° radius and marks tiles visible.
-    -- @param tile (Tile)    The tile to start at.
+    -- Cast rays in a 360° radius around a given Tile. When a ray touches a Tile
+    -- it will be set visible, explored and marked for a drawing update. If the
+    -- tile contains a WorldObject which blocks vision the ray stops there.
+    -- @param tile  (Tile)   The tile to start at.
     -- @param range (number) The view range to use.
     --
     function self:calculateVisibility( tile, range )
@@ -143,7 +188,7 @@ function Map.new()
                 end
                 target:setVisible( true );
                 target:setExplored( true );
-                target:setDirty( true ); -- Mark tile for updating.
+                target:setDirty( true );
 
                 if target:hasWorldObject() and target:getWorldObject():blocksVision() then
                     break;
@@ -159,6 +204,12 @@ function Map.new()
     -- Getters
     -- ------------------------------------------------
 
+    ---
+    -- Returns the Tile at the given coordinates.
+    -- @param x (number) The position along the x-axis.
+    -- @param y (number) The position along the y-axis.
+    -- @return  (Tile)   The Tile at the given position.
+    --
     function self:getTileAt( x, y )
         return tiles[x] and tiles[x][y];
     end
