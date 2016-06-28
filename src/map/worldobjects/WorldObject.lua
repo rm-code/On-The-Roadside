a7f0350a797ce86f6e7cae98f3a934e16f0b25e8
Robert Machmer <robert.machmer@gmail.com>
2016-06-28 11:36:11 +0200

    Clean up WorldObject.lua

--- src/map/worldobjects/WorldObject.lua
+++ src/map/worldobjects/WorldObject.lua
@@ -1,9 +1,26 @@
 local Object = require( 'src.Object' );
 
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
+
 local WorldObject = {};
 
+-- ------------------------------------------------
+-- Constructor
+-- ------------------------------------------------
+
+---
+-- Creates a new instance of the WorldObject class.
+-- @param template (table)       The WorldObject's template.
+-- @return         (WorldObject) The new WorldObject.
+--
 function WorldObject.new( template )
     local self = Object.new():addInstance( 'WorldObject' );
+
+    -- ------------------------------------------------
+    -- Private Attributes
+    -- ------------------------------------------------
 
     local name = template.name;
     local type = template.type;
@@ -21,24 +38,36 @@ function WorldObject.new( template )
     local openSprite = template.openSprite;
     local color = template.color;
 
+    -- ------------------------------------------------
+    -- Public Methods
+    -- ------------------------------------------------
+
+    ---
+    -- Reduces the WorldObject's hit points.
+    -- @param dmg (number) The amount of damage that was dealt.
+    --
     function self:damage( dmg )
         hp = hp - dmg;
     end
 
+    -- ------------------------------------------------
+    -- Getters
+    -- ------------------------------------------------
+
+    ---
+    -- Returns true if the WorldObject should be ignored during pathfinding.
+    -- @return (boolean) True if it blocks pathfinding.
+    --
     function self:blocksPathfinding()
         return blocksPathfinding;
     end
 
+    ---
+    -- Returns true if the WorldObject blocks the line of sight.
+    -- @return (boolean) True if it blocks vision.
+    --
     function self:blocksVision()
         return blocksVision;
-    end
-
-    function self:isPassable()
-        return passable;
-    end
-
-    function self:isOpenable()
-        return openable;
     end
 
     ---
@@ -49,39 +78,53 @@ function WorldObject.new( template )
         return color;
     end
 
-    function self:getSize()
-        return size;
-    end
-
-    -- TODO rename
-    function self:setBlocksVision( nblocksVision )
-        blocksVision = nblocksVision;
-    end
-
-    function self:setPassable( npassable )
-        passable = npassable;
-    end
-
+    ---
+    -- Returns the WorldObject type with which this WorldObject will be replaced
+    -- upon its destruction.
+    -- @return (string) The WorldObject type used for generating debris.
+    --
     function self:getDebrisType()
         return debrisType;
     end
 
-    function self:isDestroyed()
-        return destructible and hp <= 0 or false;
-    end
-
+    ---
+    -- Returns the WorldObject's energy reduction attribute. This is used to
+    -- reduce the amount of force a projectile has after passing through this
+    -- WorldObject.
+    -- @return (number) The WorldObject's energy reduction value.
+    --
     function self:getEnergyReduction()
         return energyReduction;
     end
 
+    ---
+    -- Returns the WorldObject's movement cost attribute.
+    -- @return (number) The amount of AP it costs to move over this WorldObject.
+    --
     function self:getMovementCost()
         return movementCost;
     end
 
+    ---
+    -- Returns the WorldObject's name.
+    -- @return (string) The WorldObject's human readable name.
+    --
     function self:getName()
         return name;
     end
 
+    ---
+    -- Returns the WorldObject's size attribute.
+    -- @return (number) The WorldObject's size.
+    --
+    function self:getSize()
+        return size;
+    end
+
+    ---
+    -- Returns the WorldObject's sprite index.
+    -- @return (number) The WorldObject's sprite index.
+    --
     function self:getSprite()
         if openable and passable then
             return openSprite;
@@ -89,12 +132,64 @@ function WorldObject.new( template )
         return sprite;
     end
 
+    ---
+    -- Returns the WorldObject's id.
+    -- @return (string) The WorldObject's id.
+    --
     function self:getType()
         return type;
     end
 
+    ---
+    -- Checks wether the WorldObject's hit points have been reduced to zero.
+    -- @return (boolean) True if the WorldObject's hit points are less or equal zero.
+    --
+    function self:isDestroyed()
+        return destructible and hp <= 0 or false;
+    end
+
+    ---
+    -- Checks wether the WorldObject is destructible.
+    -- @return (boolean) True if the WorldObject is destructible.
+    --
     function self:isDestructible()
         return destructible;
+    end
+
+    ---
+    -- Checks wether the WorldObject can be opened (i.e.: Doors, Gates, ...).
+    -- @return (boolean) True if the WorldObject is openable.
+    --
+    function self:isOpenable()
+        return openable;
+    end
+
+    ---
+    -- Checks wether the WorldObject can be passed through by a Character.
+    -- @return (boolean) True if the WorldObject is passable.
+    --
+    function self:isPassable()
+        return passable;
+    end
+
+    -- ------------------------------------------------
+    -- Setters
+    -- ------------------------------------------------
+
+    ---
+    -- Sets the WorldObject's blocksVision attribute.
+    -- @param nblocksVision (boolean) The new attribute value.
+    --
+    function self:setBlocksVision( nblocksVision )
+        blocksVision = nblocksVision;
+    end
+
+    ---
+    -- Sets the WorldObject's passable attribute.
+    -- @param npassable (boolean) The new attribute value.
+    --
+    function self:setPassable( npassable )
+        passable = npassable;
     end
 
     return self;
