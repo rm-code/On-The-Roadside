a83fc1646fd538a150ef4e275e2126de4579cd6f
Robert Machmer <robert.machmer@gmail.com>
2016-09-12 23:57:11 +0200

    Start the game in fullscreen mode by default

--- conf.lua
+++ conf.lua
@@ -26,7 +26,7 @@ function love.conf(t)
     t.window.resizable = false;
     t.window.minwidth = 1;
     t.window.minheight = 1;
-    t.window.fullscreen = false;
+    t.window.fullscreen = true;
     t.window.fullscreentype = "desktop";
     t.window.vsync = true;
     t.window.msaa = 0;
