83c15cf06098362a8494d0e1fc78eed5da8f5e19
Robert Machmer <robert.machmer@gmail.com>
2016-09-18 17:16:35 +0200

    Add text files for world objects

---
+++ res/text/de_DE/worldobjects_text.lua
@@ -0,0 +1,15 @@
+local locale = {};
+locale.identifier = 'de_DE';
+
+locale.strings = {
+    ['worldobject_chair'] = 'Stuhl';
+    ['worldobject_door'] = 'Tür';
+    ['worldobject_fence'] = 'Zaun';
+    ['worldobject_fencegate'] = 'Gartentor';
+    ['worldobject_lowwall'] = 'Niedrige Mauer';
+    ['worldobject_table'] = 'Tisch';
+    ['worldobject_wall'] = 'Mauer';
+    ['worldobject_window'] = 'Fenster';
+}
+
+return locale;
---
+++ res/text/en_EN/worldobjects_text.lua
@@ -0,0 +1,15 @@
+local locale = {};
+locale.identifier = 'en_EN';
+
+locale.strings = {
+    ['worldobject_chair'] = 'Chair';
+    ['worldobject_door'] = 'Door';
+    ['worldobject_fence'] = 'Fence';
+    ['worldobject_fencegate'] = 'Fence Gate';
+    ['worldobject_lowwall'] = 'Low Wall';
+    ['worldobject_table'] = 'Table';
+    ['worldobject_wall'] = 'Wall';
+    ['worldobject_window'] = 'Window';
+}
+
+return locale;
