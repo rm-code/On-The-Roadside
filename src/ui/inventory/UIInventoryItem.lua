ecf3bc2eca1583633a6c22325401d61c6ff57046
Robert Machmer <robert.machmer@gmail.com>
2016-09-22 11:37:51 +0200

    Fix dragging and splitting of ItemStacks
    Accidentally overwrote this with a different commit.

--- src/ui/inventory/UIInventoryItem.lua
+++ src/ui/inventory/UIInventoryItem.lua
@@ -51,8 +51,10 @@ function UIInventoryItem.new( x, y, item )
         return mouseOver;
     end
 
-    function self:drag()
-        if item:instanceOf( 'ItemStack' ) then
+    function self:drag( rmb, fullstack )
+        if item:instanceOf( 'ItemStack' ) and rmb then
+            return item:split();
+        elseif item:instanceOf( 'ItemStack' ) and not fullstack then
             return item:getItem();
         end
         return item;
