038ba4d26aafc6f2187cd5efe260872c53a2d0c0
Robert Machmer <robert.machmer@gmail.com>
2016-11-30 07:22:22 +0100

    Teach AI to use the Rearm action

---
+++ src/characters/ai/behaviortree/leafs/BTThrowingAttack.lua
@@ -0,0 +1,29 @@
+local BTLeaf = require( 'src.characters.ai.behaviortree.leafs.BTLeaf' );
+local ThrowingAttack = require( 'src.characters.actions.ThrowingAttack' );
+local Rearm = require( 'src.characters.actions.Rearm' );
+
+local BTThrowingAttack = {};
+
+function BTThrowingAttack.new()
+    local self = BTLeaf.new():addInstance( 'BTThrowingAttack' );
+
+    function self:traverse( ... )
+        print( 'BTThrowingAttack' );
+        local blackboard, character, states = ...;
+
+        character:enqueueAction( ThrowingAttack.new( character, blackboard.target ));
+
+        local weapon = character:getInventory():getBackpack():getInventory():getWeapon();
+        if weapon then
+            character:enqueueAction( Rearm.new( character, weapon:getID() ));
+        end
+
+        states:push( 'execution', character );
+
+        return true;
+    end
+
+    return self;
+end
+
+return BTThrowingAttack;
