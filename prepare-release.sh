5a09b162068f98b03115d32630cdcf5fa2400c6e
Robert Machmer <robert.machmer@gmail.com>
2017-01-15 04:37:25 +0100

    Update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -22,7 +22,7 @@ minor=${version[1]}
 patch=${version[2]}
 build=${version[3]}
 
-echo "Old Version: $major.$minor.$patch.$build"
+old="$major.$minor.$patch.$build"
 
 # Increment version based on command.
 if [ $1 == "major" ] ; then
@@ -41,9 +41,11 @@ build=$(git rev-list develop --count)
 
 formatted="$major.$minor.$patch.$build"
 
-echo "New Version: $formatted"
-
-echo "Creating a release branch using git flow ..."
+echo "####################################################"
+echo ""
+echo "    Preparing release ($old -> $formatted)"
+echo ""
+echo "####################################################"
 
 # Create a new feature branch via git flow.
 git flow release start "$formatted"
@@ -55,17 +57,26 @@ if [ ! $? -eq 0 ]; then
 fi
 
 # Add new section to changelog.
-echo "## Other Changes" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
-echo "## Fixes" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
-echo "## Removals" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
-echo "## Additions" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
-echo "# Version $formatted - $(date +"%Y-%m-%d")" | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
+echo "# Version $formatted - $(date +"%Y-%m-%d")" >> tmp_changelog
+cat CHANGELOG.md >> tmp_changelog
+mv tmp_changelog CHANGELOG.md
 
-# Replace in version.lua.
-sed -e "s/.*major =.*/    major = $major,/" version.lua | tee version.lua
-sed -e "s/.*minor =.*/    minor = $minor,/" version.lua | tee version.lua
-sed -e "s/.*patch =.*/    patch = $patch,/" version.lua | tee version.lua
-sed -e "s/.*build =.*/    build = $build,/" version.lua | tee version.lua
+# Update version.lua.
+echo "local version = {"    >> tmp_version
+echo "    major = $major,"  >> tmp_version
+echo "    minor = $minor,"  >> tmp_version
+echo "    patch = $patch,"  >> tmp_version
+echo "    build = $build,"  >> tmp_version
+echo "}"                    >> tmp_version
+mv tmp_version version.lua
 
 # Commit the changes.
 git commit -a -m "Prepare version $formatted"
+
+echo "####################################################"
+echo ""
+echo "Apply changes to README.md and CHANGELOG.md:"
+echo "    - Change Version numbers in README.md"
+echo "    - Update Changelog in CHANGELOG.md"
+echo ""
+echo "####################################################"
