08248478adf0a875f272abb78a71ae41fdc59f83
Robert Machmer <robert.machmer@gmail.com>
2017-02-04 16:26:30 +0100

    Remove superfluous check in climbing action
    This is a leftover from earlier implementations.

--- src/characters/actions/ClimbOver.lua
+++ src/characters/actions/ClimbOver.lua
@@ -6,46 +6,17 @@ local ClimbOver = {};
 function ClimbOver.new( character, target )
     local self = Action.new( target:getWorldObject():getInteractionCost(), target ):addInstance( 'ClimbOver' );
 
-    ---
-    -- Gets the direction of the target tile in relation to the character's
-    -- current location.
-    -- @param neighbours (table)  A table containing all the neighbouring tiles.
-    -- @return           (string) The direction of the tile.
-    --
-    local function findDirection( neighbours )
-        for dir, neighbour in pairs( neighbours ) do
-            if neighbour == target then
-                return dir;
-            end
-        end
-    end
-
-    ---
-    -- Checks if the tile behind the WorldObject is free to move to.
-    -- @param toCheck (Tile)    The tile to check.
-    -- @return        (boolean) True if it is a valid tile.
-    --
-    local function isValidTarget( toCheck )
-        return toCheck:isPassable() and not toCheck:isOccupied() and not toCheck:hasWorldObject();
-    end
-
     function self:perform()
         local current = character:getTile();
 
         assert( target:isAdjacent( current ), 'Character has to be adjacent to the target tile!' );
 
-        local direction = findDirection( current:getNeighbours() );
-        local tileBehindObject = target:getNeighbours()[direction];
+        current:removeCharacter();
+        target:addCharacter( character );
+        character:setTile( target );
 
-        if isValidTarget( tileBehindObject ) then
-            current:removeCharacter();
-            target:addCharacter( character );
-            character:setTile( target );
-
-            Messenger.publish( 'SOUND_CLIMB' );
-            return true;
-        end
-        return false;
+        Messenger.publish( 'SOUND_CLIMB' );
+        return true;
     end
 
     return self;
