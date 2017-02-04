5d178a37e8f0d804f89d1432899049b31753bbc9
Robert Machmer <robert.machmer@gmail.com>
2017-02-04 23:34:22 +0100

    Add a new more tactical map
    Closes #95.

---
+++ res/data/tiles/Gravel.lua
@@ -0,0 +1,7 @@
+return {
+    id = 'tile_gravel',
+    sprite = 250,
+    color = { 105, 106, 106 },
+    movementCost = 1,
+    passable = true
+}
---
+++ res/data/tiles/WoodenFloor.lua
@@ -0,0 +1,7 @@
+return {
+    id = 'tile_woodenfloor',
+    sprite = 173,
+    color = { 102, 57, 49 },
+    movementCost = 1,
+    passable = true
+}
