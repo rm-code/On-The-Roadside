65150ecaaa2327f6e8c83fb6f55c577dc7784b48
Robert Machmer <robert.machmer@gmail.com>
2017-02-07 01:21:23 +0100

    Clean up AttackInput.lua

--- src/turnbased/helpers/AttackInput.lua
+++ src/turnbased/helpers/AttackInput.lua
@@ -4,40 +4,54 @@ local MeleeAttack = require( 'src.characters.actions.MeleeAttack' );
 local ThrowingAttack = require( 'src.characters.actions.ThrowingAttack' );
 local Rearm = require( 'src.characters.actions.Rearm' );
 
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
+
 local AttackInput = {};
+
+-- ------------------------------------------------
+-- Constructor
+-- ------------------------------------------------
 
 function AttackInput.new()
     local self = State.new():addInstance( 'AttackInput' );
 
-    local function generateAttack( target, character )
-        character:enqueueAction( Attack.new( character, target ));
-    end
-
-    function self:request( ... )
-        local target, character = ...;
-
+    ---
+    -- Requests a new action for a given character.
+    -- @param target    (Tile)      The tile to act upon.
+    -- @param character (Character) The character to create the action for.
+    -- @return          (boolean)   True if an action was created, false otherwise.
+    --
+    function self:request( target, character )
         -- Prevent characters from attacking themselves.
         if target == character:getTile() then
             return false;
         end
 
         local weapon = character:getWeapon();
+
+        -- Characters can't attack with no weapon equipped.
         if not weapon then
             return false;
         end
 
+        -- Handle Melee weapons.
         if weapon:getWeaponType() == 'Melee' then
             character:enqueueAction( MeleeAttack.new( character, target ));
-            return true;
         end
 
+        -- Handle Thrown weapons.
         if weapon:getWeaponType() == 'Thrown' then
             character:enqueueAction( ThrowingAttack.new( character, target ));
             character:enqueueAction( Rearm.new( character, weapon:getID() ));
-            return true;
         end
 
-        generateAttack( target, character );
+        -- Handle Ranged weapons.
+        if weapon:getWeaponType() == 'Ranged' then
+            character:enqueueAction( Attack.new( character, target ));
+        end
+
         return true;
     end
 
