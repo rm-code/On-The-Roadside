08a79b1cfb157307c98b9f4f6c1b4854ffb189cf
Robert Machmer <robert.machmer@gmail.com>
2017-02-07 21:47:04 +0100

    Move stance modifiers to character class

--- src/characters/actions/Walk.lua
+++ src/characters/actions/Walk.lua
@@ -4,22 +4,11 @@ local Messenger = require( 'src.Messenger' );
 local Walk = {};
 
 -- ------------------------------------------------
--- Constants
--- ------------------------------------------------
-
-local STANCES = require('src.constants.Stances');
-local STANCE_MODIFIER = {
-    [STANCES.STAND]  = 1.0,
-    [STANCES.CROUCH] = 1.5,
-    [STANCES.PRONE]  = 2.0,
-}
-
--- ------------------------------------------------
 -- Constructor
 -- ------------------------------------------------
 
 function Walk.new( character, target )
-    local self = Action.new( math.floor( target:getMovementCost() * STANCE_MODIFIER[character:getStance()] ), target ):addInstance( 'Walk' );
+    local self = Action.new( math.floor( target:getMovementCost() * character:getStanceModifier() ), target ):addInstance( 'Walk' );
 
     function self:perform()
         local current = character:getTile();
