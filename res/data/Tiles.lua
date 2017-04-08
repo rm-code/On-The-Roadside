da9f80b90da1cdc4d9cbc2b9a903d0b6b2808e29
Robert Machmer <robert.machmer@gmail.com>
2017-04-08 14:40:19 +0200

    Move tile templates to a common file

---
+++ res/data/Tiles.lua
@@ -0,0 +1,74 @@
+return {
+    {
+        id = 'tile_asphalt',
+        sprite = 47,
+        color = { 89, 86, 82 },
+        movementCost = {
+            stand  = 1,
+            crouch = 1,
+            prone  = 2,
+        },
+        passable = true
+    },
+    {
+        id = 'tile_deep_water',
+        sprite = 248,
+        color = { 63, 63, 116 },
+        passable = false
+    },
+    {
+        id = 'tile_grass',
+        sprite = 45,
+        color = { 75, 105, 47 },
+        movementCost = {
+            stand  = 1,
+            crouch = 1,
+            prone  = 2,
+        },
+        passable = true
+    },
+    {
+        id = 'tile_gravel',
+        sprite = 250,
+        color = { 105, 106, 106 },
+        movementCost = {
+            stand  = 1,
+            crouch = 1,
+            prone  = 2,
+        },
+        passable = true
+    },
+    {
+        id = 'tile_soil',
+        sprite = 45,
+        color = { 102, 57, 49 },
+        movementCost = {
+            stand  = 1,
+            crouch = 1,
+            prone  = 2,
+        },
+        passable = true
+    },
+    {
+        id = 'tile_water',
+        sprite = 248,
+        color = { 48, 96, 130 },
+        movementCost = {
+            stand  = 3,
+            crouch = 4,
+            prone  = 6,
+        },
+        passable = true
+    },
+    {
+        id = 'tile_woodenfloor',
+        sprite = 173,
+        color = { 102, 57, 49 },
+        movementCost = {
+            stand  = 1,
+            crouch = 1,
+            prone  = 2,
+        },
+        passable = true
+    }
+}
