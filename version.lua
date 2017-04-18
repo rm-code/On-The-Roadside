0b76ed1a2ad8161e2db1484fdddb587eacf26474
Robert Machmer <robert.machmer@gmail.com>
2017-04-18 18:05:14 +0200

    Prepare version 0.9.0.1014

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 local version = {
     major = 0,
-    minor = 8,
-    patch = 2,
-    build = 976,
+    minor = 9,
+    patch = 0,
+    build = 1014,
 }
 
 return string.format( "%d.%d.%d.%d", version.major, version.minor, version.patch, version.build );
