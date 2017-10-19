3e2815921345665d61576e1ee0909b578ec7a00c
Robert Machmer <robert.machmer@gmail.com>
2017-10-19 18:14:25 +0200

    Add test prefabs for procedural generation

---
+++ res/data/maps/prefabs/test_big/info.lua
@@ -0,0 +1,19 @@
+return {
+    name = 'test_big',
+    parcelsize = {
+        width  = 2,
+        height = 2
+    },
+    ground = {
+        { r =   0, g =   0, b =   0, id = 'tile_asphalt'     },
+        { r = 102, g =  57, b =  49, id = 'tile_woodenfloor' }
+    },
+    objects = {
+        { r = 143, g =  86, b =  59, id = 'worldobject_wall'   },
+        { r =  99, g = 155, b = 255, id = 'worldobject_window' },
+        { r = 223, g = 113, b =  38, id = 'worldobject_door'   },
+        { r = 105, g = 106, b = 106, id = 'worldobject_chair'  },
+        { r =  89, g =  86, b =  82, id = 'worldobject_table'  },
+        { r = 238, g = 195, b = 154, id = 'worldobject_crate'  }
+    }
+}
