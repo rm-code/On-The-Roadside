433eae983a767fc013fab6661470c511843324ce
Robert Machmer <robert.machmer@gmail.com>
2017-10-28 22:34:29 +0200

    Add new map layouts
    Closes #187.

---
+++ res/data/procgen/layouts/small.lua
@@ -0,0 +1,277 @@
+return {
+  dimensions = {
+    height = 6,
+    width = 8
+  } --[[table: 0x09be67b8]],
+  parcels = {
+    foliage = {
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 0
+      } --[[table: 0x09bec320]],
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 1
+      } --[[table: 0x09bec3b0]],
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 2
+      } --[[table: 0x09bec440]],
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 3
+      } --[[table: 0x09bec4d0]],
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 4
+      } --[[table: 0x09bec560]],
+      {
+        h = 1,
+        w = 1,
+        x = 0,
+        y = 5
+      } --[[table: 0x09bec5f0]],
+      {
+        h = 1,
+        w = 1,
+        x = 2,
+        y = 0
+      } --[[table: 0x09bec680]],
+      {
+        h = 1,
+        w = 1,
+        x = 2,
+        y = 5
+      } --[[table: 0x09bec710]],
+      {
+        h = 1,
+        w = 1,
+        x = 3,
+        y = 0
+      } --[[table: 0x09bec7a0]],
+      {
+        h = 1,
+        w = 1,
+        x = 3,
+        y = 5
+      } --[[table: 0x09bec8c0]],
+      {
+        h = 1,
+        w = 1,
+        x = 4,
+        y = 0
+      } --[[table: 0x09bec950]],
+      {
+        h = 1,
+        w = 1,
+        x = 4,
+        y = 5
+      } --[[table: 0x09bec9e0]],
+      {
+        h = 1,
+        w = 1,
+        x = 5,
+        y = 0
+      } --[[table: 0x09beca70]],
+      {
+        h = 1,
+        w = 1,
+        x = 5,
+        y = 5
+      } --[[table: 0x09becb00]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 0
+      } --[[table: 0x09becb90]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 5
+      } --[[table: 0x09becc20]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 0
+      } --[[table: 0x09beccb0]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 1
+      } --[[table: 0x09bece50]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 2
+      } --[[table: 0x09becee0]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 3
+      } --[[table: 0x09becf70]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 4
+      } --[[table: 0x09bed000]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 5
+      } --[[table: 0x09bed090]]
+    } --[[table: 0x09be6920]],
+    huge = {
+      {
+        h = 2,
+        w = 4,
+        x = 2,
+        y = 2
+      } --[[table: 0x09bec880]]
+    } --[[table: 0x09be69c0]],
+    large = {} --[[table: 0x09be6998]],
+    medium = {} --[[table: 0x09be6970]],
+    road = {
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 0
+      } --[[table: 0x0d5aa638]],
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 1
+      } --[[table: 0x0d5aa748]],
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 2
+      } --[[table: 0x0d5ab9e8]],
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 3
+      } --[[table: 0x0d5ab960]],
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 4
+      } --[[table: 0x0d5aa970]],
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 5
+      } --[[table: 0x0d5aaa50]],
+      {
+        h = 1,
+        w = 1,
+        x = 2,
+        y = 1
+      } --[[table: 0x0d5aaae0]],
+      {
+        h = 1,
+        w = 1,
+        x = 2,
+        y = 4
+      } --[[table: 0x0d5aab70]],
+      {
+        h = 1,
+        w = 1,
+        x = 3,
+        y = 1
+      } --[[table: 0x0d5aac00]],
+      {
+        h = 1,
+        w = 1,
+        x = 3,
+        y = 4
+      } --[[table: 0x0d5ab230]],
+      {
+        h = 1,
+        w = 1,
+        x = 4,
+        y = 1
+      } --[[table: 0x0d5ab2c0]],
+      {
+        h = 1,
+        w = 1,
+        x = 4,
+        y = 4
+      } --[[table: 0x0d5ab448]],
+      {
+        h = 1,
+        w = 1,
+        x = 5,
+        y = 1
+      } --[[table: 0x0d5ab4d8]],
+      {
+        h = 1,
+        w = 1,
+        x = 5,
+        y = 4
+      } --[[table: 0x0d5ab568]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 1
+      } --[[table: 0x0d5ab5f8]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 2
+      } --[[table: 0x0d5ab688]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 3
+      } --[[table: 0x0d5ab878]],
+      {
+        h = 1,
+        w = 1,
+        x = 6,
+        y = 4
+      } --[[table: 0x0d5ab908]],
+      {
+        h = 1,
+        w = 1,
+        x = 7,
+        y = 1
+      } --[[table: 0x0d5ab208]]
+    } --[[table: 0x09be68f8]],
+    small = {} --[[table: 0x09be6948]],
+    spawns = {
+      {
+        h = 1,
+        w = 1,
+        x = 1,
+        y = 1
+      } --[[table: 0x09be6e50]]
+    } --[[table: 0x09be68d0]]
+  } --[[table: 0x09be67e0]]
+} --[[table: 0x09be6758]]
