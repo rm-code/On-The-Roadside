d96aa1754b293c25056ae184b065d6ae02e5da94
Robert Machmer <robert.machmer@gmail.com>
2017-11-03 08:23:05 +0100

    Refactor MousePointer module

--- src/ui/MousePointer.lua
+++ src/ui/MousePointer.lua
@@ -1,42 +1,62 @@
+---
+-- @module MousePointer
+--
+
+-- ------------------------------------------------
+-- Required Modules
+-- ------------------------------------------------
+
 local TexturePacks = require( 'src.ui.texturepacks.TexturePacks' )
+
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
 
 local MousePointer = {}
 
-local camera;
+-- ------------------------------------------------
+-- Private Variables
+-- ------------------------------------------------
 
-local mx, my;
-local gx, gy;
-local wx, wy;
+local camera
+
+local mx, my
+local gx, gy
+local wx, wy
 local tw, th
 
+-- ------------------------------------------------
+-- Public Functions
+-- ------------------------------------------------
+
 function MousePointer.init( ncamera )
-    camera = ncamera;
+    camera = ncamera
 
     tw, th = TexturePacks.getTileDimensions()
 
-    love.mouse.setPosition( love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5 );
+    love.mouse.setPosition( love.graphics.getWidth() * 0.5, love.graphics.getHeight() * 0.5 )
 
-    mx, my = love.mouse.getPosition();
-    wx, wy = camera:worldCoords( mx, my );
+    mx, my = love.mouse.getPosition()
+    wx, wy = camera:worldCoords( mx, my )
     gx, gy = math.floor( mx / tw ), math.floor( my / th )
 end
 
 function MousePointer.update()
-    mx, my = camera:mousepos();
-    wx, wy = camera:worldCoords( love.mouse.getPosition() );
+    mx, my = camera:mousepos()
+    wx, wy = camera:worldCoords( love.mouse.getPosition() )
     gx, gy = math.floor( mx / tw ), math.floor( my / th )
 end
 
 function MousePointer.clear()
-    camera = nil;
+    camera = nil
 end
 
 function MousePointer.getWorldPosition()
-    return wx, wy;
+    return wx, wy
 end
 
 function MousePointer.getGridPosition()
-    return gx, gy;
+    return gx, gy
 end
 
-return MousePointer;
+return MousePointer
