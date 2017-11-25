e0cab5e22a51230bc2a8f89543f6396852d6330e
Robert Machmer <robert.machmer@gmail.com>
2017-11-25 12:35:48 +0100

    Remove manual conversion of numerical table keys
    This isn't necessary with the usage of Ser / Compressor anymore.

--- src/SaveHandler.lua
+++ src/SaveHandler.lua
@@ -28,37 +28,6 @@ local VERSION_FILE = 'version.data'
 -- ------------------------------------------------
 
 ---
--- Takes care of transforming strings to numbers if possible.
--- @param value (mixed) The value to check.
--- @return      (mixed) The converted value.
---
-local function convertStrings( value )
-    local keysToReplace = {};
-
-    for k, v in pairs( value ) do
-        if tonumber( k ) then
-            keysToReplace[#keysToReplace + 1] = k;
-        end
-
-        if type( v ) == 'table' then
-            convertStrings( v );
-        elseif tonumber( v ) then
-            value[k] = tonumber( v );
-        end
-    end
-
-    -- If the key can be transformed into a number delete the original
-    -- key-value pair and store the value with the numerical key.
-    for _, k in ipairs( keysToReplace ) do
-        local v = value[k];
-        value[k] = nil;
-        value[tonumber(k)] = v;
-    end
-
-    return value;
-end
-
----
 -- Creates a file containing only the version string.
 -- @string dir     The directory to store the version file in.
 -- @table  version A table containing the version field.
@@ -90,7 +59,7 @@ function SaveHandler.save( t, name )
 end
 
 function SaveHandler.load( path )
-    return convertStrings( Compressor.load( path .. '/' .. COMPRESSED_SAVE ))
+    return Compressor.load( path .. '/' .. COMPRESSED_SAVE )
 end
 
 function SaveHandler.loadVersion( path )
