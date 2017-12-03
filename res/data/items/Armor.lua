f3a87dcf784868f73381d22c704f24de4a04da56
Robert Machmer <robert.machmer@gmail.com>
2017-12-03 19:13:08 +0100

    Refactor Armor.lua

--- res/data/items/Armor.lua
+++ res/data/items/Armor.lua
@@ -1,92 +1,92 @@
 return {
     {
-        id         = "footwear_combat_boots",
-        idDesc     = "footwear_combat_boots_desc",
-        itemType   = "Armor",
-        weight     = 3,
-        volume     = 2.3,
+        id = "footwear_combat_boots",
+        idDesc = "footwear_combat_boots_desc",
+        itemType = "Armor",
+        weight = 3,
+        volume = 2.3,
         equippable = true,
-        stackable  = false,
-        permanent  = false,
-        subType    = "Footwear",
-        armor = {
-            protection = 12,
-            coverage   = 100
-        },
+        stackable = false,
+        permanent = false,
         tags = {
             'humanoid'
+        },
+        subType = "Footwear",
+        armor = {
+            protection = 12,
+            coverage = 100
         }
     },
     {
-        id         = "headgear_pasgt_helmet",
-        idDesc     = "headgear_pasgt_helmet_desc",
-        itemType   = "Armor",
-        weight     = 1.3,
-        volume     = 3.1,
+        id = "headgear_pasgt_helmet",
+        idDesc = "headgear_pasgt_helmet_desc",
+        itemType = "Armor",
+        weight = 1.3,
+        volume = 3.1,
         equippable = true,
-        stackable  = false,
-        permanent  = false,
-        subType    = "Headgear",
+        stackable = false,
+        permanent = false,
+        tags = {
+            'humanoid'
+        },
+        subType = "Headgear",
         armor = {
             protection = 30,
-            coverage   = 45
-        },
-        tags = {
-            'humanoid'
+            coverage = 45
         }
     },
     {
-        id         = "jacket_pasgt_vest",
-        idDesc     = "jacket_pasgt_vest_desc",
-        itemType   = "Armor",
-        weight     = 4,
-        volume     = 4.3,
+        id = "jacket_pasgt_vest",
+        idDesc = "jacket_pasgt_vest_desc",
+        itemType = "Armor",
+        weight = 4,
+        volume = 4.3,
         equippable = true,
-        stackable  = false,
-        permanent  = false,
-        subType    = "Jacket",
+        stackable = false,
+        permanent = false,
+        tags = {
+            'humanoid'
+        },
+        subType = "Jacket",
         armor = {
             protection = 12,
-            coverage   = 80
-        },
-        tags = {
-            'humanoid'
+            coverage = 80
         }
     },
     {
-        id         = "trousers_jeans",
-        idDesc     = "trousers_jeans_desc",
-        itemType   = "Armor",
-        weight     = 1.0,
-        volume     = 0.5,
+        id = "trousers_jeans",
+        idDesc = "trousers_jeans_desc",
+        itemType = "Armor",
+        weight = 1.0,
+        volume = 0.5,
         equippable = true,
-        stackable  = false,
-        permanent  = false,
-        subType    = "Trousers",
-        armor = {
-            protection = 4,
-            coverage   = 100
-        },
+        stackable = false,
+        permanent = false,
         tags = {
             'humanoid'
-        }
-    },
-    {
-        id         = "thick_fur",
-        idDesc     = "thick_fur_desc",
-        itemType   = "Armor",
-        weight     = 1.0,
-        volume     = 0.5,
-        equippable = true,
-        stackable  = false,
-        permanent  = true,
-        subType    = "Fur",
+        },
+        subType = "Trousers",
         armor = {
             protection = 4,
-            coverage   = 100
+            coverage = 100
         },
+    },
+    {
+        id = "thick_fur",
+        idDesc = "thick_fur_desc",
+        itemType = "Armor",
+        weight = 1.0,
+        volume = 0.5,
+        equippable = true,
+        stackable = false,
+        permanent = true,
         tags = {
             'creature'
+        },
+        subType = "Fur",
+        armor = {
+            protection = 4,
+            coverage = 100
         }
     }
 }
