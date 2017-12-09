63ecda3bad06f587c35d6ec248462e2dc078cbdc
Robert Machmer <robert.machmer@gmail.com>
2017-12-09 01:21:24 +0100

    Refactor SavegameScreen

--- src/ui/screens/SavegameScreen.lua
+++ src/ui/screens/SavegameScreen.lua
@@ -2,7 +2,11 @@
 -- @module SavegameScreen
 --
 
-local Screen = require( 'lib.screenmanager.Screen' )
+-- ------------------------------------------------
+-- Required Modules
+-- ------------------------------------------------
+
+local Screen = require( 'src.ui.screens.Screen' )
 local Translator = require( 'src.util.Translator' )
 local ScreenManager = require( 'lib.screenmanager.ScreenManager' )
 local TexturePacks = require( 'src.ui.texturepacks.TexturePacks' )
@@ -18,7 +22,7 @@ local Util = require( 'src.util.Util' )
 -- Module
 -- ------------------------------------------------
 
-local SavegameScreen = {}
+local SavegameScreen = Screen:subclass( 'SavegameScreen' )
 
 -- ------------------------------------------------
 -- Constants
@@ -42,167 +46,143 @@ local BUTTON_LIST_WIDTH = 20
 local BUTTON_LIST_Y = 20
 
 -- ------------------------------------------------
--- Constructor
+-- Private Functions
 -- ------------------------------------------------
 
-function SavegameScreen.new()
-    local self = Screen.new()
-
-    -- ------------------------------------------------
-    -- Private Variables
-    -- ------------------------------------------------
-
-    local title
-    local buttonList
-    local font
-    local footer
-    local container
-
-    -- ------------------------------------------------
-    -- Private Functions
-    -- ------------------------------------------------
-
-    local function createTitle()
-        title = love.graphics.newText( font:get() )
-        for i, line in ipairs( TITLE_STRING ) do
-            local coloredtext = {}
-            for w in string.gmatch( line, '.' ) do
-                if w == '@' then
-                    coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_1' )
-                    coloredtext[#coloredtext + 1] = 'O'
-                elseif w == '!' then
-                    coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_2' )
-                    coloredtext[#coloredtext + 1] = w
-                else
-                    coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_3' )
-                    coloredtext[#coloredtext + 1] = w
-                end
-                title:add( coloredtext, 0, i * font:get():getHeight() )
+local function createTitle()
+    local font = TexturePacks.getFont():get()
+    local title = love.graphics.newText( font )
+    for i, line in ipairs( TITLE_STRING ) do
+        local coloredtext = {}
+        for w in string.gmatch( line, '.' ) do
+            if w == '@' then
+                coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_1' )
+                coloredtext[#coloredtext + 1] = 'O'
+            elseif w == '!' then
+                coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_2' )
+                coloredtext[#coloredtext + 1] = w
+            else
+                coloredtext[#coloredtext + 1] = TexturePacks.getColor( 'ui_title_3' )
+                coloredtext[#coloredtext + 1] = w
             end
+            title:add( coloredtext, 0, i * font:getHeight() )
+        end
+    end
+    return title
+end
+
+local function drawTitle( title )
+    local cx, _ = GridHelper.centerElement( GridHelper.pixelsToGrid( title:getWidth(), title:getHeight() * #TITLE_STRING ))
+    local tw, _ = TexturePacks.getTileDimensions()
+    love.graphics.draw( title, cx * tw, TITLE_POSITION * TexturePacks.getFont():getGlyphHeight() )
+end
+
+local function createBackButton( lx, ly )
+    local function callback()
+        ScreenManager.switch( 'mainmenu' )
+    end
+    return UIButton( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1, callback, Translator.getText( 'ui_back' ))
+end
+
+local function createSaveGameEntry( lx, ly, index, item, folder )
+    local version = SaveHandler.loadVersion( folder )
+
+    -- Generate the string for the savegame button showing the name of the saves,
+    -- the version of the game at which they were created and their creation date.
+    local str = string.format( '%2d. %s', index, item )
+    str = Util.rightPadString( str, 36, ' ')
+    str = str .. string.format( '  %s    %s', version, os.date( '%Y-%m-%d  %X', love.filesystem.getLastModified( folder )))
+
+    local function callback()
+        if version == getVersion() then
+            local save = SaveHandler.load( folder )
+            ScreenManager.switch( 'gamescreen', save )
         end
     end
 
-    local function drawTitle()
-        local cx, _ = GridHelper.centerElement( GridHelper.pixelsToGrid( title:getWidth(), title:getHeight() * #TITLE_STRING ))
-        local tw, _ = TexturePacks.getTileDimensions()
-        love.graphics.draw( title, cx * tw, TITLE_POSITION * TexturePacks.getFont():getGlyphHeight() )
-    end
-
-    local function createBackButton( lx, ly )
-        local function callback()
-            ScreenManager.switch( 'mainmenu' )
-        end
-        return UIButton( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1, callback, Translator.getText( 'ui_back' ))
-    end
-
-    local function createSaveGameEntry( lx, ly, index, item, folder )
-        local version = SaveHandler.loadVersion( folder )
-
-        -- Generate the string for the savegame button showing the name of the saves,
-        -- the version of the game at which they were created and their creation date.
-        local str = string.format( '%2d. %s', index, item )
-        str = Util.rightPadString( str, 36, ' ')
-        str = str .. string.format( '  %s    %s', version, os.date( '%Y-%m-%d  %X', love.filesystem.getLastModified( folder )))
-
-        local function callback()
-            if version == getVersion() then
-                local save = SaveHandler.load( folder )
-                ScreenManager.switch( 'gamescreen', save )
-            end
-        end
-
-        local button = UIButton( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1, callback, str, 'center' )
-        button:setActive( version == getVersion() )
-        return button
-    end
+    local button = UIButton( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1, callback, str, 'center' )
+    button:setActive( version == getVersion() )
+    return button
+end
 
 
-    local function createButtons()
-        local lx = GridHelper.centerElement( BUTTON_LIST_WIDTH, 1 )
-        local ly = BUTTON_LIST_Y
+local function createButtons()
+    local lx = GridHelper.centerElement( BUTTON_LIST_WIDTH, 1 )
+    local ly = BUTTON_LIST_Y
 
-        buttonList = UIVerticalList( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1 )
+    local buttonList = UIVerticalList( lx, ly, 0, 0, BUTTON_LIST_WIDTH, 1 )
 
-        -- Create entries for last five savegames.
-        local items = love.filesystem.getDirectoryItems( SaveHandler.getSaveFolder() )
-        local counter = 0
+    -- Create entries for last five savegames.
+    local items = love.filesystem.getDirectoryItems( SaveHandler.getSaveFolder() )
+    local counter = 0
 
-        for i = #items, 1, -1 do
-            local item = items[i]
-            if love.filesystem.isDirectory( SaveHandler.getSaveFolder() .. '/' .. item ) then
-                counter = counter + 1
-                buttonList:addChild( createSaveGameEntry( lx, ly, counter, item, SaveHandler.getSaveFolder() .. '/' .. item ))
-            end
-        end
-
-        buttonList:addChild( createBackButton( lx, ly ))
-    end
-
-
-    -- ------------------------------------------------
-    -- Public Methods
-    -- ------------------------------------------------
-
-    function self:init()
-        font = TexturePacks.getFont()
-
-        createTitle()
-        createButtons()
-
-        container = UIContainer()
-        container:register( buttonList )
-
-        footer = UICopyrightFooter.new()
-    end
-
-    function self:update()
-        font = TexturePacks.getFont()
-        container:update()
-    end
-
-    function self:draw()
-        font:use()
-        drawTitle()
-
-        container:draw()
-
-        footer:draw()
-    end
-
-    function self:keypressed( _, scancode )
-        love.mouse.setVisible( false )
-
-        if scancode == 'escape' then
-            ScreenManager.switch( 'mainmenu' )
-        end
-
-        if scancode == 'up' then
-            container:command( 'up' )
-        elseif scancode == 'down' then
-            container:command( 'down' )
-        elseif scancode == 'return' then
-            container:command( 'activate' )
+    for i = #items, 1, -1 do
+        local item = items[i]
+        if love.filesystem.isDirectory( SaveHandler.getSaveFolder() .. '/' .. item ) then
+            counter = counter + 1
+            buttonList:addChild( createSaveGameEntry( lx, ly, counter, item, SaveHandler.getSaveFolder() .. '/' .. item ))
         end
     end
 
-    function self:mousemoved()
-        love.mouse.setVisible( true )
+    buttonList:addChild( createBackButton( lx, ly ))
+    return buttonList
+end
+
+-- ------------------------------------------------
+-- Public Methods
+-- ------------------------------------------------
+
+function SavegameScreen:initialize()
+    self.title = createTitle()
+    self.buttonList = createButtons()
+
+    self.container = UIContainer()
+    self.container:register( self.buttonList )
+
+    self.footer = UICopyrightFooter.new()
+end
+
+function SavegameScreen:update()
+    self.container:update()
+end
+
+function SavegameScreen:draw()
+    drawTitle( self.title )
+    self.container:draw()
+    self.footer:draw()
+end
+
+function SavegameScreen:keypressed( _, scancode )
+    love.mouse.setVisible( false )
+
+    if scancode == 'escape' then
+        ScreenManager.switch( 'mainmenu' )
     end
 
-    ---
-    -- Handle mousereleased events.
-    --
-    function self:mousereleased()
-        container:mousecommand( 'activate' )
+    if scancode == 'up' then
+        self.container:command( 'up' )
+    elseif scancode == 'down' then
+        self.container:command( 'down' )
+    elseif scancode == 'return' then
+        self.container:command( 'activate' )
     end
+end
 
-    function self:resize( _, _ )
-        local lx = GridHelper.centerElement( BUTTON_LIST_WIDTH, 1 )
-        local ly = BUTTON_LIST_Y
-        buttonList:setOrigin( lx, ly )
-    end
+function SavegameScreen:mousemoved()
+    love.mouse.setVisible( true )
+end
 
-    return self
+---
+-- Handle mousereleased events.
+--
+function SavegameScreen:mousereleased()
+    self.container:mousecommand( 'activate' )
+end
+
+function SavegameScreen:resize( _, _ )
+    local lx = GridHelper.centerElement( BUTTON_LIST_WIDTH, 1 )
+    local ly = BUTTON_LIST_Y
+    self.buttonList:setOrigin( lx, ly )
 end
 
 return SavegameScreen
