fdc0671a5dd0920825b60490cec9d5041b568987
Robert Machmer <robert.machmer@gmail.com>
2017-12-18 01:04:40 +0100

    Refactor BTMustReload

--- src/characters/ai/behaviortree/leafs/BTMustReload.lua
+++ src/characters/ai/behaviortree/leafs/BTMustReload.lua
@@ -1,20 +1,30 @@
-local Log = require( 'src.util.Log' );
-local BTLeaf = require( 'src.characters.ai.behaviortree.leafs.BTLeaf' );
+---
+-- @module BTMustReload
+--
 
-local BTMustReload = {};
+-- ------------------------------------------------
+-- Required Modules
+-- ------------------------------------------------
 
-function BTMustReload.new()
-    local self = BTLeaf.new():addInstance( 'BTMustReload' );
+local Log = require( 'src.util.Log' )
+local BTLeaf = require( 'src.characters.ai.behaviortree.leafs.BTLeaf' )
 
-    function self:traverse( ... )
-        local _, character = ...;
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
 
-        local result = character:getWeapon():getMagazine():isEmpty();
-        Log.debug( result, 'BTMustReload' );
-        return result;
-    end
+local BTMustReload = BTLeaf:subclass( 'BTMustReload' )
 
-    return self;
+-- ------------------------------------------------
+-- Public Methods
+-- ------------------------------------------------
+
+function BTMustReload:traverse( ... )
+    local _, character = ...
+
+    local result = character:getWeapon():getMagazine():isEmpty()
+    Log.debug( result, 'BTMustReload' )
+    return result
 end
 
-return BTMustReload;
+return BTMustReload
