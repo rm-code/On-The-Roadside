5d64be439d600842fd805dee589391dba9b962da
Robert Machmer <robert.machmer@gmail.com>
2017-12-19 13:58:35 +0100

    Refactor PathOverlay

--- src/ui/overlays/PathOverlay.lua
+++ src/ui/overlays/PathOverlay.lua
@@ -1,13 +1,14 @@
 ---
 -- The PathOverlay module draws the movement path of the currently selected
 -- character.
+-- @module PathOverlay
 --
 
 -- ------------------------------------------------
 -- Required Modules
 -- ------------------------------------------------
 
-local Object = require( 'src.Object' )
+local Class = require( 'lib.Middleclass' )
 local TexturePacks = require( 'src.ui.texturepacks.TexturePacks' )
 local MovementInput = require( 'src.turnbased.helpers.MovementInput' )
 
@@ -15,67 +16,67 @@ local MovementInput = require( 'src.turnbased.helpers.MovementInput' )
 -- Module
 -- ------------------------------------------------
 
-local PathOverlay = {}
+local PathOverlay = Class( 'PathOverlay' )
 
 -- ------------------------------------------------
--- Constructor
+-- Private Methods
 -- ------------------------------------------------
 
-function PathOverlay.new( game, pulser )
-    local self = Object.new():addInstance( 'PathOverlay' )
-
-    -- ------------------------------------------------
-    -- Private Methods
-    -- ------------------------------------------------
-
-    ---
-    -- Selects a color for the node in a path based on the distance to the
-    -- target and the remaining action points the character has.
-    -- @param value (number) The cost of the node.
-    -- @param total (number) The total number of nodes in the path.
-    --
-    local function selectPathNodeColor( value, total )
-        local fraction = value / total
-        if fraction < 0 then
-            return TexturePacks.getColor( 'ui_path_ap_low' )
-        elseif fraction <= 0.2 then
-            return TexturePacks.getColor( 'ui_path_ap_med' )
-        elseif fraction <= 0.6 then
-            return TexturePacks.getColor( 'ui_path_ap_high' )
-        elseif fraction <= 1.0 then
-            return TexturePacks.getColor( 'ui_path_ap_full' )
-        end
+---
+-- Selects a color for the node in a path based on the distance to the
+-- target and the remaining action points the character has.
+-- @param value (number) The cost of the node.
+-- @param total (number) The total number of nodes in the path.
+--
+local function selectPathNodeColor( value, total )
+    local fraction = value / total
+    if fraction < 0 then
+        return TexturePacks.getColor( 'ui_path_ap_low' )
+    elseif fraction <= 0.2 then
+        return TexturePacks.getColor( 'ui_path_ap_med' )
+    elseif fraction <= 0.6 then
+        return TexturePacks.getColor( 'ui_path_ap_high' )
+    elseif fraction <= 1.0 then
+        return TexturePacks.getColor( 'ui_path_ap_full' )
     end
+end
 
-    -- ------------------------------------------------
-    -- Public Methods
-    -- ------------------------------------------------
+-- ------------------------------------------------
+-- Public Methods
+-- ------------------------------------------------
 
-    ---
-    -- Draws a path for this character.
-    --
-    function self:draw()
-        local tw, th = TexturePacks.getTileDimensions()
-        local character = game:getCurrentCharacter()
-        local mode = game:getState():getInputMode()
-        if mode:isInstanceOf( MovementInput ) and mode:hasPath() then
-            local total = character:getActionPoints()
-            local ap = total
-            mode:getPath():iterate( function( tile )
-                ap = ap - tile:getMovementCost( character:getStance() )
+---
+-- Initializes the PathOverlay object.
+-- @tparam Game   game   The game object.
+-- @tparam Pulser pulser The pulser used for animating the overlay.
+--
+function PathOverlay:initialize( game, pulser )
+    self.game = game
+    self.pulser = pulser
+end
 
-                -- Draws the path overlay.
-                love.graphics.setBlendMode( 'add' )
-                local color = selectPathNodeColor( ap, total )
-                love.graphics.setColor( color[1], color[2], color[3], pulser:getPulse() )
-                love.graphics.rectangle( 'fill', tile:getX() * tw, tile:getY() * th, tw, th )
-                TexturePacks.resetColor()
-                love.graphics.setBlendMode( 'alpha' )
-            end)
-        end
+---
+-- Draws a path for this character.
+--
+function PathOverlay:draw()
+    local tw, th = TexturePacks.getTileDimensions()
+    local character = self.game:getCurrentCharacter()
+    local mode = self.game:getState():getInputMode()
+    if mode:isInstanceOf( MovementInput ) and mode:hasPath() then
+        local total = character:getActionPoints()
+        local ap = total
+        mode:getPath():iterate( function( tile )
+            ap = ap - tile:getMovementCost( character:getStance() )
+
+            -- Draws the path overlay.
+            love.graphics.setBlendMode( 'add' )
+            local color = selectPathNodeColor( ap, total )
+            love.graphics.setColor( color[1], color[2], color[3], self.pulser:getPulse() )
+            love.graphics.rectangle( 'fill', tile:getX() * tw, tile:getY() * th, tw, th )
+            TexturePacks.resetColor()
+            love.graphics.setBlendMode( 'alpha' )
+        end)
     end
-
-    return self
 end
 
 return PathOverlay
