5bcc13b0553712b866b5eaa678fad7c0e7299d6f
Robert Machmer <robert.machmer@gmail.com>
2017-12-19 14:12:39 +0100

    Refactor Item

--- src/items/Item.lua
+++ src/items/Item.lua
@@ -1,64 +1,86 @@
-local Object = require( 'src.Object' );
+---
+-- @module Item
+--
 
-local Item = {};
+-- ------------------------------------------------
+-- Required Modules
+-- ------------------------------------------------
 
-function Item.new( template )
-    local self = Object.new():addInstance( 'Item' );
+local Class = require( 'lib.Middleclass' )
 
-    function self:getID()
-        return template.id;
-    end
+-- ------------------------------------------------
+-- Module
+-- ------------------------------------------------
 
-    function self:getDescriptionID()
-        return template.idDesc or 'default_item_description';
-    end
+local Item = Class( 'Item' )
 
-    function self:getItemType()
-        return template.itemType;
-    end
+-- ------------------------------------------------
+-- Public Methods
+-- ------------------------------------------------
 
-    function self:getSubType()
-        return template.subType;
-    end
-
-    function self:isSameType( itemType, subType )
-        if template.itemType == itemType then
-            if not subType or subType == template.subType then
-                return true;
-            end
-        end
-        return false;
-    end
-
-    function self:getWeight()
-        return template.weight;
-    end
-
-    function self:getVolume()
-        return template.volume;
-    end
-
-    function self:isEquippable()
-        return template.equippable;
-    end
-
-    function self:isStackable()
-        return template.stackable;
-    end
-
-    function self:isPermanent()
-        return template.permanent;
-    end
-
-    function self:serialize()
-        local t = {
-            ['id'] = template.id,
-            ['itemType'] = template.itemType
-        };
-        return t;
-    end
-
-    return self;
+function Item:initialize( template )
+    self.id = template.id
+    self.descriptionID = template.idDesc
+    self.itemType = template.itemType
+    self.subType = template.subType
+    self.weight = template.weight
+    self.volume = template.volume
+    self.equippable = template.equippable
+    self.stackable = template.stackable
+    self.permanent = template.permanent
 end
 
-return Item;
+function Item:getID()
+    return self.id
+end
+
+function Item:getDescriptionID()
+    return self.descriptionID or 'default_item_description'
+end
+
+function Item:getItemType()
+    return self.itemType
+end
+
+function Item:getSubType()
+    return self.subType
+end
+
+function Item:isSameType( itemType, subType )
+    if self.itemType == itemType then
+        if not subType or subType == self.subType then
+            return true
+        end
+    end
+    return false
+end
+
+function Item:getWeight()
+    return self.weight
+end
+
+function Item:getVolume()
+    return self.volume
+end
+
+function Item:isEquippable()
+    return self.equippable
+end
+
+function Item:isStackable()
+    return self.stackable
+end
+
+function Item:isPermanent()
+    return self.permanent
+end
+
+function Item:serialize()
+    local t = {
+        ['id'] = self.id,
+        ['itemType'] = self.itemType
+    }
+    return t
+end
+
+return Item
