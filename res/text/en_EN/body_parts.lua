d4110b83d29b39917e39eb3eaf545f334f573fe9
Robert Machmer <robert.machmer@gmail.com>
2018-01-15 01:00:52 +0100

    Remove unused body part templates

--- res/text/en_EN/body_parts.lua
+++ res/text/en_EN/body_parts.lua
@@ -3,31 +3,8 @@ locale.identifier = 'en_EN';
 
 locale.strings = {
     ['head'] = "Head",
-    ['brain'] = "Brain",
-    ['ears'] = "Ears",
-    ['eyes'] = "Eyes",
-    ['nose'] = "Nose",
-    ['skull'] = "Skull",
-    ['throat'] = "Throat",
-
-    ['arm_left'] = "Arm (L)",
-    ['hand_left'] = "Hand (L)",
-    ['arm_right'] = "Arm (R)",
-    ['hand_right'] = "Hand (R)",
-
-    ['leg_left'] = "Leg (L)",
-    ['foot_left'] = "Foot (L)",
-    ['leg_right'] = "Leg (R)",
-    ['foot_right'] = "Foot (R)",
-
     ['legs'] = "Legs",
-
     ['torso'] = "Torso",
-    ['heart'] = "Heart",
-    ['kidneys'] = "Kidneys",
-    ['liver'] = "Liver",
-    ['lungs'] = "Lungs",
-    ['ribcage'] = "Ribcage",
 
     ['equip_head'] = "Head",
     ['equip_mouth'] = "Mouth",
