969493c2c4ca8b6934bb6566babc0d9df3ca8642
Robert Machmer <robert.machmer@gmail.com>
2018-01-19 12:30:57 +0100

    Remove unused templates

--- res/text/en_EN/messages_text.lua
+++ res/text/en_EN/messages_text.lua
@@ -5,9 +5,6 @@ locale.strings = {
     ['msg_hit_indestructible_worldobject'] = "Projectile hits %s",
     ['msg_hit_worldobject'] = "Projectile perforates %s and damages it for %d points.",
     ['msg_hit_character'] = "Projectile hits %s.",
-    ['msg_hit_bodypart'] = "Attack enters body at %s.",
-    ['msg_hit_armor'] = "%s absorbs %d points of damage.",
-    ['msg_dmg_bodypart'] = "%s is hit for %s points of %s damage. New HP: %s.",
     ['msg_character_death'] = "%s dies.",
     ['msg_character_no_ap_left'] = "Not enough action points left.",
 }
