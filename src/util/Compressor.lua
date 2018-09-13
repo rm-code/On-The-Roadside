dec10d50b22a1d25db6f7ab227db1334dc88577f
Robert Machmer <robert.machmer@gmail.com>
2018-09-13 11:37:36 +0200

    Update documentation

--- src/util/Compressor.lua
+++ src/util/Compressor.lua
@@ -1,4 +1,7 @@
 ---
+-- The Compressor module takes care of serializing, compressing and saving data
+-- to the harddisk, as well as loading and deserializing / decompressing it again.
+--
 -- @module Compressor
 --
 
@@ -15,29 +18,42 @@ local Serialize = require( 'lib.Ser' )
 local Compressor = {}
 
 -- ------------------------------------------------
--- Constructor
+-- Public Functions
 -- ------------------------------------------------
 
+---
+-- Serializes and compresses the provided data.
+-- Throws an error if the file can't be written to disk.
+-- @tparam table  t    The data table to serialize and compress.
+-- @tparam string path The path to save the serialized data at.
+--
 function Compressor.save( t, path )
     local rawstring = Serialize( t )
     local compressedData = love.data.compress( 'data', 'lz4', rawstring )
     assert( love.filesystem.write( path, compressedData ))
 end
 
+---
+-- Deserializes and decompresses a file and returns the data table.
+-- Throws an error if the loading fails.
+-- @tparam  string path The path to load the serialized data from.
+-- @treturn table       The loaded data.
+--
 function Compressor.load( path )
     -- Load file and throw an error if something went wrong.
     local compressedData, errorMessage = love.filesystem.read( path )
     assert( compressedData, errorMessage )
 
-    -- Decompress and return the loaded lua file.
+    -- Decompress the loaded Lua file to a string.
     local rawstring = love.data.decompress( 'string', 'lz4', compressedData )
 
-    -- Print a warning if it can't be loaded and return false.
+    -- Return the error code if something went wrong while loading the Lua code.
     local result, error = load( rawstring )
     if not result then
         return result, error
     end
 
+    -- Load and execute the actual Lua code.
     return load( rawstring )()
 end
 
