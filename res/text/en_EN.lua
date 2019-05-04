939cdd54e578dddff20b964b5424c57002b77d85
Robert Machmer <robert.machmer@gmail.com>
2019-05-05 01:37:58 +0200

    Remove unused message strings

--- res/text/en_EN.lua
+++ res/text/en_EN.lua
@@ -130,8 +130,6 @@ locale.strings = {
     -- ==============================
     -- MESSAGES
     -- ==============================
-    ['msg_hit_indestructible_worldobject'] = "Projectile hits %s",
-    ['msg_hit_worldobject'] = "Projectile perforates %s and damages it for %d points",
     ['msg_body_hit'] = "The attack hits the creature's %s and deals %d points of damage",
     ['msg_character_no_ap_left'] = "Not enough AP left",
     ['msg_status_effect'] = "Creature is %s",
